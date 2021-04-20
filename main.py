a00cbf751af402972a2eda36fa22287585b6ad1c
MILAN ZINZUVADIYA <milanzinzuvadiya99@gmail.com>
2021-04-20 04:51:12 -0400

    add methods for prediction

--- main.py
+++ main.py
@@ -6,8 +6,20 @@ import matplotlib.pyplot as plt
 from stock_predict import *
 
 @st.cache
-def stock_predict(ticker,test_pr):
+def stock_predict_auto(ticker,test_pr):
     return run(ticker,test_pr)
+
+@st.cache
+def stock_predict_arima(ticker,test_pr):
+    return run(ticker,test_pr,'arima')
+
+@st.cache
+def stock_predict_ses(ticker,test_pr):
+    return run(ticker,test_pr,'ses')
+
+@st.cache
+def stock_predict_es(ticker,test_pr):
+    return run(ticker,test_pr,'es')
 
 st.write("""
 # Stock Prediction App
@@ -17,19 +29,58 @@ This app predicts the **Stock Prices** !
 st.sidebar.header('Prediction Setting')
 
 def user_input_features():
-    test_pr = st.sidebar.slider('Test data percentage %', 1, 20, 10)
-    stock = st.sidebar.text_input("Enter ticker of stock", "NFLX")
-    data = {'test_pr': test_pr,
-            'stock': stock}
-    features = pd.DataFrame(data, index=[0])
-    return features
+    usr_par = {}
+    usr_par['test_pr'] = st.sidebar.slider('Test data percentage %', 1, 20, 10)
+    usr_par['stock'] = st.sidebar.text_input("Enter ticker of stock", "NFLX")
+
+    list_algo = ['AutoRegression','Moving Average','Simple Exponential Smoothing','Exponential Smoothing']
+    selected_algo = []
+    for i in list_algo:
+        selected_algo.append(st.sidebar.checkbox(i))
+    usr_par['selected_algo'] = selected_algo
+    seasons = 0
+    
+    usr_par['seasons'] = seasons
+    
+    return usr_par
 
 df = user_input_features()
-st.subheader('User Input parameters')
-st.table(df)
 
-predict_data,train = stock_predict(df['stock'][0],df['test_pr'][0])
-predict_data = predict_data[["Adj Close","AutoRegression"]]
+compare_graph = pd.DataFrame()
 
-st.line_chart(predict_data)
+# 0 index is for autoregression 
+if df['selected_algo'][0]:
+    predict_data,train = stock_predict_auto(df['stock'],df['test_pr'])
+    compare_graph = predict_data
+
+# 1 index is for sarimax
+if df['selected_algo'][1]:
+    predict_data, train = stock_predict_arima(df['stock'],df['test_pr'])
+    compare_graph = pd.concat([compare_graph,predict_data],axis=1)
+
+# 2 index is for Simple Exponential Smoothing
+if df['selected_algo'][2]:
+    predict_data, train = stock_predict_ses(df['stock'],df['test_pr'])
+    compare_graph = pd.concat([compare_graph,predict_data],axis=1)
+
+# 3 index is for Exponential Smoothing
+if df['selected_algo'][3]:
+    predict_data, train = stock_predict_es(df['stock'],df['test_pr'])
+    compare_graph = pd.concat([compare_graph,predict_data],axis=1)
+
+st.subheader('Prediction of ' + str(df['test_pr'])+' % data of '+df['stock'])
+st.line_chart(compare_graph)
 st.line_chart(train['Adj Close'])
+
+st.subheader('HI')
+
+#dtr = pd.DataFrame(train['Adj Close']).append(compare_graph,sort=False)
+dtr = pd.concat([train[['Adj Close']],compare_graph],axis=1,sort=False)
+st.write(dtr)
+st.line_chart(dtr)
+
+#dtf = predict_data.append(pd)
+#fg = pd.concat([predict_data_auto,predict_data_arima],axis=1)
+
+
+#st.write(dtf)
--- stock_predict.py
+++ stock_predict.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from statsmodels.tsa.ar_model import AutoReg
 from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 predict_label = 'Adj Close'
 
@@ -25,14 +28,25 @@ def train_test(test_pr,ticker):
 #Forecast
 def train_auto(train,test):
     y_hat = test.copy()
-    forecast_name= 'AutoRegression'
     model = AutoReg(train['Adj Close'],lags=1)
     model_fit = model.fit()
     return model_fit,y_hat
 
-def train_sarimax(n_seasons,train):
+def train_arimax(train,test):
     y_hat = test.copy()
-    model = SARIMAX(train[predict_label],order=(1,1,1),seasonal_order=(1,1,1,n_seasons))
+    model = ARIMA(train[predict_label],order=(5,1,1))
+    model_fit = model.fit()
+    return model_fit,y_hat
+
+def train_ses(train,test):
+    y_hat = test.copy()
+    model = SimpleExpSmoothing(train[predict_label])
+    model_fit = model.fit()
+    return model_fit,y_hat
+
+def train_es(train,test):
+    y_hat = test.copy()
+    model = ExponentialSmoothing(train[predict_label])
     model_fit = model.fit()
     return model_fit,y_hat
 
@@ -40,16 +54,22 @@ def predict(model,train,test,y_hat):
     return np.array(model.predict(start=len(train),end=len(train)+len(test)-1))
 #prediction
 
-def run(ticker,test_pr,model_name='auto',n_seasons=4):
+def run(ticker,test_pr,model_name='auto'):
     train,test = train_test(test_pr,ticker)
     if model_name == 'auto':
         model,y_hat = train_auto(train,test)
         forecast_name = 'AutoRegression'
+    elif model_name == 'ses':
+        model,y_hat = train_ses(train,test)
+        forecast_name = 'Simple-Exp-Smoothing'
+    elif model_name == 'es':
+        model,y_hat = train_es(train,test)
+        forecast_name = 'Expo-Smoothing'
     else:
-        model,y_hat = train_sarimax(n_seasons,train)
-        forecast_name = 'SARIMAX'
+        model,y_hat = train_arimax(train,test)
+        forecast_name = 'Moving Average'
     
     y_hat[forecast_name] = predict(model,train,test,y_hat)
-    return y_hat,train
+    return y_hat[["Adj Close",forecast_name]],train
     
 
